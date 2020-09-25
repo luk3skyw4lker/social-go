5b3185e242a477c5bc501904c6b8e14975935624
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2020-09-25 20:09:24 -0300

    Adding custom responses

---
+++ api/responses/json.go
@@ -0,0 +1,33 @@
+package responses
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// JSON is...
+func JSON(res http.ResponseWriter, statusCode int, data interface{}) {
+	res.WriteHeader(statusCode)
+
+	err := json.NewEncoder(res).Encode(data)
+
+	if err != nil {
+		fmt.Fprintf(res, "%s", err.Error())
+	}
+}
+
+// ERROR is..
+func ERROR(res http.ResponseWriter, statusCode int, err error) {
+	if err != nil {
+		JSON(res, statusCode, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		})
+
+		return
+	}
+
+	JSON(res, http.StatusBadRequest, nil)
+}
